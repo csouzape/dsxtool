20f406b3657753369b00c0e82feb211edcb92722
Carlos Gabriel <oditorrinco222@gmail.com>
2026-05-19 10:36:46 -0300

    fix: adjust indentation for KDE Plasma option in desktop prompt

--- modules/change_desktop.sh
+++ modules/change_desktop.sh
@@ -156,7 +156,7 @@ prompt_change_desktop() {
     )
 
     local options=(
-        "󰧨  KDE Plasma"
+        "  KDE Plasma"
         "  XFCE"
         "  Hyprland"
         "  Hyprland (csouzape edition)"

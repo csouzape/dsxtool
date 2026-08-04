97f38e0b121827ef50a76dc903de2dab56a7df3e
Carlos Gabriel <csouzadsx@protonmail.com>
2026-08-03 23:53:35 -0300

    fix: remove bug from dsxswap not starting

--- install.sh
+++ install.sh
@@ -239,7 +239,7 @@ dsxtool_main() {
             "Setup Virtualization")        clear; install_virtualization_module ;;
             "Setup Shell")                 clear; install_shell_module ;;
             "Setup Gaming")                clear; setup_gaming_module ;;
-            "DSXSWAP (BETA)")              clear; dsxswap_module ;;
+            "DSXSwap")                     clear; dsxswap_module ;;
             "DSXSecurity")                 clear; dsxsecurity_module ;;
             "Setup Bluetooth")             clear; bluetooth_module ;;
             "Setup Printer")               clear; setup_printer_module ;;
